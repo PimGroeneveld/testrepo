135e65a80b942a321b0a3b32657666b963ac09af
Pim <pimgroeneveld92@gmail.com>
2018-07-17 14:40:34 +0100

    initial commit adds add subtract multiply

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,15 @@
+def return_10()
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply (first_number, second_number)
+  return first_number * second_number
+end
