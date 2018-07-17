77726d9dc426eafa3d4f65673ef3735d01d52647
Pim <pimgroeneveld92@gmail.com>
2018-07-17 15:05:43 +0100

    adds full month

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,18 @@ end
 def add_string_as_number (x, y)
   return x.to_i + y.to_i
 end
+
+def number_to_full_month_name (x)
+
+case x
+when 1
+    return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+  else
+    return "fail"
+  end
+
+end
